174e6e22b925a92a60b51ce75784a452238c6e31
arthathagary <arthathagary@gmail.com>
2025-02-08 23:46:13 +0530

    Add .gitignore, update package.json, and implement basic server setup with MongoDB connection

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("🚀 Vehicle Reservation API Running..."));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
